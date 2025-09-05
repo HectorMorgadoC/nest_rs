f22e05f0bf5e9157f8638a79b5fd17d78960b24a
Hector Morgado RS1 <e3.corefloriambra@proton.me>
2025-09-05 11:37:43 -0500

    feat(file): Add content type validation for file uploads
    This commit introduces content type validation to the  function in the file helper. The validation ensures that only files with the following MIME types are allowed:
    
    - image/png
    - image/jpg
    - image/jpeg
    - image/gif
    - image/webp
    
    If a file with an unsupported content type is uploaded, a  is returned to the client. This enhances the security and robustness of the file upload functionality.

--- src/app/shared/helper/file_helper.rs
+++ src/app/shared/helper/file_helper.rs
@@ -1,16 +1,39 @@
 pub mod file_filter {
 
-    use std::io::Write;
+    use std::{fmt::format, io::Write};
 
     use actix_multipart::Multipart;
     use actix_web::{Error, HttpResponse, web};
     use futures_util::TryStreamExt;
 
     pub async fn load_file(mut payload: Multipart) -> Result<HttpResponse, Error> {
-        std::fs::create_dir_all("./static/upload")?; // ← Cambio aquí
-        println!("Static/upload directory verified");
+        std::fs::create_dir_all("./static/upload")?;
+        let mut is_right_type = false;
 
-        while let Some(mut field) = payload.try_next().await.unwrap() {
+        while let Some(mut field) = payload.try_next().await? {
+            let content_type = field.content_type();
+
+            if let Some(value) = content_type {
+                let type_image_list = [
+                    "image/png",
+                    "image/jpg",
+                    "image/jpeg",
+                    "image/gif",
+                    "image/webp",
+                ];
+                let content_type_str = value.essence_str();
+                for image in type_image_list {
+                    if content_type_str == image {
+                        is_right_type = true;
+                    };
+                }
+            }
+
+            if !is_right_type {
+                return Ok(HttpResponse::BadRequest()
+                    .body("The file does not correspond to the image extension"));
+            };
+
             if let Some(file_name) = field.content_disposition().expect("REASON").get_filename() {
                 let file_path = format!("./static/upload/{file_name}");
                 println!("Saving: {}", file_path.clone());
